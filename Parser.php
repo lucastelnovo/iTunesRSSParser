1bcb7b2284b7ce1158f561332d393ae6e50547e6
Lucas <lucastelnovo@hotmail.com>
2013-06-27 12:01:43 -0700

    Agrego un par de archivos mas y el parcial del createMap()

--- Parser.php
+++ Parser.php
@@ -3,7 +3,8 @@
 class Parser {
 	
 	private $urlRssDoc; // Lista con los rss a convertir a formato iTunes
-	private $elementsMap; // Lista en forma de strings con elemementos obligatorios
+	private $elementsList; // Lista en forma de strings con elemementos obligatorios
+	private $elementsMap; // Mapa con (elemento, array)
 	private $defaultInformationMap; // Mapa con (elemento, valor_default)
 	
 
@@ -11,16 +12,28 @@ class Parser {
 		
 		$this->urlRssDoc = $urlRssDoc;
 		$this->defaultInformationMap = $defaultInformationMap;
-		
-		$this->createElementsMap ( $elementsList );
+		$this->elementsList = $elementsList;
+		$this->elementsMap = $this->createElementsMap ( $this->elementsList );
 	
 	}
 	
-	public function createElementsMap($elementsList) {
+	public function createElementsMap() {
+		
 		/*
+		 * @INFO
 		 * for each ($string en la lista elementsList)
 		 *		agregame un elemento al mapa con key = $string y contenido con un array vacio.
 		 */
+		
+		$keys = array(); // Defino un array en el que guardo los elementos que seran las keys del mapa
+		
+		foreach ( $this->elementsList as $string ) {
+
+			$keys[] = $string; // Completo el array con todos los elementos poniendolos como keys
+		
+		}
+		
+		return array_fill_keys ( $keys, $array = array () ); // A cada key le doy de valor un array vacio
 	
 	}
 	
@@ -28,7 +41,7 @@ class Parser {
 	public function extractElements() {
 		
 		/*
-		 * 
+		 * @INFO
 		 * foreach($elemento de la lista de elementos)
 		 * 		$miElemento = $rss->channel->$elemento
 		 * 		foreach($miElemento as $descripcionDeElementoActual)
