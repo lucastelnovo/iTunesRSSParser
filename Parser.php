2e5538680471e810be9de24a2cb033a7efd0ce20
preller <patricioyr@gmail.com>
2013-06-28 07:39:06 -0700

    Parser modificado: creado el template comentado y a medio codear de los nuevos métodos.

--- Parser.php
+++ Parser.php
@@ -10,7 +10,7 @@ class Parser {
 
 	public function __construct($urlsRss, $elementsList, $defaultInformationMap) {
 		
-		$this->$urlsRss = $urlsRss;
+		$this->urlsRss = $urlsRss;
 		$this->defaultInformationMap = $defaultInformationMap;
 		$this->elementsList = $elementsList;
 		$this->elementsMap = $this->createElementsMap ();
@@ -18,47 +18,87 @@ class Parser {
 	}
 	
 	public function createElementsMap() {
-		
 		/*
-		 * @INFO
-		 * for each ($string en la lista elementsList)
-		 *		agregame un elemento al mapa con key = $string y contenido con un array vacio.
+		 * Crea el mapa de elementos, con key = element y contenido = array vacío.
 		 */
 		
-		$keys = array(); // Defino un array en el que guardo los elementos que seran las keys del mapa
+		$keys = array (); // Defino un array en el que guardo los elementos que seran las keys del mapa
 		
+
 		foreach ( $this->elementsList as $string ) {
-			
 			$emptyArray = array ();
-			$keys[$string] = $emptyArray; // Completo el array con todos los elementos poniendolos como keys
-		
+			$keys [$string] = $emptyArray; // Completo el array con todos los elementos poniendolos como keys
 		}
 		
 		return $keys;
 	}
 	
+	public function createValidFeed() {
+		//escupe los headers al nuevo rss
+		$this->newFeed .= "<rss xmlns:atom=\"http://www.w3.org/2005/Atom\" xmlns:wfw=\"http://wellformedweb.org/CommentAPI/\" xmlns:dc=\"http://purl.org/dc/elements/1.1/\" xmlns:dcterms=\"http://purl.org/dc/terms/\" xmlns:content=\"http://purl.org/rss/1.0/modules/content/\" xmlns:itunes=\"http://www.itunes.com/dtds/podcast-1.0.dtd\" version=\"2.0\">" . "\n";
+		
+		//foreach archivo pasado a parsear
+		foreach ( $urlsRss as $url ) {
+			
+			//abre el channel
+			$this->newFeed .= "<channel>";
+			
+			//extrae la info al mapa
+			$this->extractElementsContent ( $url );
+			
+			//escupe la info del mapa al nuevo rss
+			$this->addElementsContent ();
+			
+			//limpia el mapa
+			foreach ( array_keys ( $this->elementsMap ) as $key ) {
+				//TODO
+			}
+			
+			//cierra el channel
+			$newFeed .= "</channel>";
+			
+			//returnea el nuevo feed
+			return $newFeed;
+		}
+	}
 	
-	// Para cada documento extrae la descripcion/contenido de los elementos 
-	public function extractElements() {
+	public function extractElementsContent($url) {
+		// Para cada documento extrae la descripcion/contenido de los elementos 
 		
-		/*
-		 * @INFO
-		 * foreach($elemento de la lista de elementos)
-		 * 		$miElemento = $rss->channel->$elemento
-		 * 		foreach($miElemento as $descripcionDeElementoActual)
-		 * 			addToList($elemento->listaDeContenidos, $descripcionDeElementoActual)
-		 * 
-		 */
-		
-		$rss = simplexml_load_file ( $this->urlRssDoc );
-		
-		if ($rss) {
-			echo '<h1>' . $rss->channel->title . '</h1>';
-			echo '<li>' . $rss->channel->link . '</li>';
-			$items = $rss->channel->item;
+
+		//foreach elemento de la lista
+		foreach ( $this->elementsMap as $element ) {
 		
 		}
 	
+		//busca el element del mapa en el archivo y agregá su contenido al array del mapa
+	
+
+	//
+	
+
+	/*
+			$rss = simplexml_load_file($url);
+			if($rss)
+				{
+				echo '<h1>'.$rss->channel->title.'</h1>';
+				echo '<li>'.$rss->channel->pubDate.'</li>';
+				$items = $rss->channel->item;
+				foreach($items as $item)
+				{
+			$title = $item->title;
+			$link = $item->link;
+			$published_on = $item->pubDate;
+			$description = $item->description;
+			echo '<h3><a href="'.$link.'">'.$title.'</a></h3>';
+			echo '<span>('.$published_on.')</span>';
+			echo '<p>'.$description.'</p>';
+			}
+	*/
+	}
+
+	public function addElementsContent(){
+		
 	}
 
 }
